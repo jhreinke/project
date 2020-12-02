2ab92596d5340e1ceb0fe4ffa17a056e81126152
Jeferson Heinemann Reinke <reinke@MacBook-Pro-de-Jeferson.local>
2020-12-01 23:13:33 -0300

    Alteração arquivo principal

--- src/main/java/com/semoral/api/ProjectApplication.java
+++ src/main/java/com/semoral/api/ProjectApplication.java
@@ -3,11 +3,14 @@ package com.semoral.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 @SpringBootApplication
 public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
+		System.out.print("Teste ok");
 	}
 
 }
